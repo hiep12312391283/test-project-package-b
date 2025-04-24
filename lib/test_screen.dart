2c5b7fe290869e9a716f56b552b285e62dc3d825
Hiep <macbookpro@MacBook-Pro-cua-Macbook.local>
2025-04-24 09:48:13 +0700

    abc

---
+++ lib/test_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+        ],
+      ),
+    );
+  }
+}
